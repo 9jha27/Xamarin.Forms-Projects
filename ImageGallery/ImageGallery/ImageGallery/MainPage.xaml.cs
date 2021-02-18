24228bd6dfcbfc1da6682a0d912715b6cea83775
Laura <9jha27@gmail.com>
2021-02-17 17:11:08 -0700

    Consolidate Folder

---
+++ ImageGallery/ImageGallery/ImageGallery/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageGallery
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _currentImageId = 143;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _currentImageId = 143;
+
+            LoadImage();
+        }
+
+        void LoadImage()
+        {
+            image.Source = new UriImageSource 
+            {
+                Uri = new Uri(String.Format("https://picsum.photos/id/{0}/200/300", _currentImageId)),
+                CachingEnabled = false
+            };
+
+        }    
+
+        private void previous_Clicked(object sender, EventArgs e)
+        {
+            _currentImageId--;
+
+            LoadImage();
+        }
+
+        private void next_Clicked(object sender, EventArgs e)
+        {
+            _currentImageId++;
+
+            LoadImage();
+        }
+    }
+}
---
+++ ImageGallery/ImageGallery/ImageGallery/MarkupExtensions/ImageId.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageGallery.MarkupExtensions
+{
+    [ContentProperty("ImageId")]
+    class ImageId : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
